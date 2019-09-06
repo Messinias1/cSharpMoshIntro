48e627f7b8fe40c315bb93b635b26512fa9acf6b
Carl Kakisis <Messinias1@gmail.com>
2019-09-06 14:41:54 -0400

    type conversion

--- HelloWorld/helloworld.cs
+++ HelloWorld/helloworld.cs
@@ -14,6 +14,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // variable types
+
             //byte number = 2;
             //int count = 10;
             //float totalPrice = 20.95f;
@@ -21,19 +23,63 @@ namespace HelloWorld
             //string firstName = "Carl";
             //bool isWorking = true;
 
-            var number = 2;
-            var count = 10;
-            var totalPrice = 20.95f;
-            var character = 'A';
-            var firstName = "Carl";
-            var isWorking = true;
+            // var keyword
 
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
+            //var number = 2;
+            //var count = 10;
+            //var totalPrice = 20.95f;
+            //var character = 'A';
+            //var firstName = "Carl";
+            //var isWorking = true;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(count);
+            //Console.WriteLine(totalPrice);
+            //Console.WriteLine(character);
+            //Console.WriteLine(firstName);
+            //Console.WriteLine(isWorking);
+
+            // placeholders and values
+
+            //Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            //Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            // const keyword
+
+            //const float Pi = 3.14f;
+
+            // implicit declaration
+
+            //byte b = 1;
+            //int i = b;
+            //Console.WriteLine(i);
+
+            // explicit declaration (can cause overflow)
+
+            //int i = 1000;
+            //byte b = (byte)i;
+            //Console.WriteLine(b);
+
+            // TYPE CONVERSION
+
+            //var number = "1234";
+            //var number2 = "2";
+            //int i = Convert.ToInt32(number);
+            //int j = Convert.ToInt32(number2);
+            //Console.WriteLine(i * j);
+
+            // UNSUCCESSFUL CONVERSION USING A TRY
+
+            //try
+            //{
+            //    var num = "1234";
+            //    byte b = Convert.ToByte(num);
+            //    Console.WriteLine(b);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("The number could not be converted to a byte");
+            //}
         }
     }
 }
