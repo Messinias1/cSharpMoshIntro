fde70daf5b482346d7db31958801c807370c3df5
Carl Kakisis <Messinias1@gmail.com>
2019-09-06 13:33:45 -0400

    var and console writeline

--- HelloWorld/helloworld.cs
+++ HelloWorld/helloworld.cs
@@ -2,11 +2,38 @@
 
 namespace HelloWorld
 {
+    //class helloworld
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello World!");
+    //    }
+    //}
+
     class helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //byte number = 2;
+            //int count = 10;
+            //float totalPrice = 20.95f;
+            //char character = 'A';
+            //string firstName = "Carl";
+            //bool isWorking = true;
+
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Carl";
+            var isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
         }
     }
 }
